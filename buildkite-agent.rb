0b140a08011a923c0a08cdf339cab6cfecc6ae15
Buildkite Agent Releaser <61678375+buildkite-agent-releaser@users.noreply.github.com>
2023-01-20 09:35:19 +1100

    buildkite-agent 3.43.1

--- buildkite-agent.rb
+++ buildkite-agent.rb
@@ -3,13 +3,13 @@ class BuildkiteAgent < Formula
   homepage "https://buildkite.com/docs/agent"
 
   stable do
-    version "3.43.0"
+    version "3.43.1"
     if Hardware::CPU.arm?
-      url     "https://github.com/buildkite/agent/releases/download/v3.43.0/buildkite-agent-darwin-arm64-3.43.0.tar.gz"
-      sha256  "8ae1c0aff70add64f1849b4d3fa804ac129947660e0e5708dc22ce6737116826"
+      url     "https://github.com/buildkite/agent/releases/download/v3.43.1/buildkite-agent-darwin-arm64-3.43.1.tar.gz"
+      sha256  "5e898328207eaaf7858deb8165cdfe0ef4f5f52e364571188dbff483a3c8b0a7"
     else
-      url     "https://github.com/buildkite/agent/releases/download/v3.43.0/buildkite-agent-darwin-amd64-3.43.0.tar.gz"
-      sha256  "d2bfb994e712398eb3cde306f8f668b50c8e30d77284fbbffd456693199574ab"
+      url     "https://github.com/buildkite/agent/releases/download/v3.43.1/buildkite-agent-darwin-amd64-3.43.1.tar.gz"
+      sha256  "7d18e973ebf057526c6d8c5c3dc92a1ebb45d148bc3b3d9daec0e87d02e3cc2b"
     end
   end
 

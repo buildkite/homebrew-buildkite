2d21afb292088dae333f1704ed2adbb94ae7ccc0
Samuel Cochran <sj26@sj26.com>
2024-07-02 17:06:59 +1000

    Update to 0.7.1 and test version command

--- test-splitter.rb
+++ test-splitter.rb
@@ -2,13 +2,13 @@ class TestSplitter < Formula
   desc "Buildkite test splitting client"
   homepage "https://github.com/buildkite/test-splitter"
 
-  version "0.7.0"
+  version "0.7.1"
   if Hardware::CPU.arm?
-    url "https://github.com/buildkite/test-splitter/releases/download/v0.7.0/test-splitter-darwin-arm64"
-    sha256 "d651ee82885675f57cab6eef3f82980b0c3e554af1f92f2aff9ec31177482b56"
+    url "https://github.com/buildkite/test-splitter/releases/download/v0.7.1/test-splitter-darwin-arm64"
+    sha256 "f67dda69a3e2b15dc5f2d0a6aa5bde71b4b0526b918eaaef9746af57218af4f2"
   else
-    url "https://github.com/buildkite/test-splitter/releases/download/v0.7.0/test-splitter-darwin-amd64"
-    sha256 "f6fa9f54652921c21b650514d21747a81557df83a711b354508a75331ad555ca"
+    url "https://github.com/buildkite/test-splitter/releases/download/v0.7.1/test-splitter-darwin-amd64"
+    sha256 "2af04832cb323bd5b10dba4d7334908cf9ceefa185fb0866ebbb90149f17d599"
   end
 
   def install
@@ -20,10 +20,6 @@ class TestSplitter < Formula
   end
 
   test do
-    # We should be able to do this, but it currently fails
-    # https://github.com/buildkite/test-splitter/issues/106
-    # system "test-splitter", "--version"
-
-    assert_predicate bin/"test-splitter", :executable?
+    system "test-splitter", "--version"
   end
 end
